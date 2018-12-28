94c7f2a42ef898132567bf7fc71bee5bfd9769f2
Vadim <vadim.galaktionov@gmail.com>
2018-12-29 00:43:43 +0100

    fix deploy

--- cmd.sh
+++ cmd.sh
@@ -6,6 +6,7 @@ function deploy() {
     docker-compose build
     docker-compose push
     eval $(docker-machine env curatify-do)
+    docker-compose pull
     docker-compose up -d --no-build
     eval $(docker-machine env -u)
 }
