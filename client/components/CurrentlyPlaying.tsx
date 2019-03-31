933eb695a36b330b35627492228b1e4148fbae0a
Vadim <vadim.galaktionov@gmail.com>
2019-03-31 18:17:57 +0200

    animate things

--- client/components/CurrentlyPlaying.tsx
+++ client/components/CurrentlyPlaying.tsx
@@ -20,11 +20,16 @@ export default function CurrentlyPlaying({ track }: CurrentlyPlayingProps) {
   return (
     <div className="column is-12 has-text-centered currently-playing">
       <img src={imageUrl} style={{ height: "280px" }} />
-      <h5 className="is-size-5 has-text-weight-semibold track-name">{track.name}</h5>
+      <h5 className="is-size-5 has-text-weight-semibold track-name ticker name is-full-width">
+        {track.name}
+      </h5>
+      <br />
       {track.artists && (
-        <p className="has-text-weight-semibold">{track.artist_names.join(", ")} </p>
+        <p className="has-text-weight-semibold ticker artists is-full-width">
+          {track.artist_names.join(", ")}
+        </p>
       )}
-      {track.track_id && <SelectMatchingPlaylist track={track} />}
+      <div>{track.track_id && <SelectMatchingPlaylist track={track} />}</div>
     </div>
   );
 }
